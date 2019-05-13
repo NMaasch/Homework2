f272471ad457f56c1308b54d5ac9892a3e28ea20
NMaasch <nathanmaasch@gmail.com>
2019-05-12 22:38:18 -0700

    Add files via upload

---
+++ Assets/Scripts/RenderEffectBloom.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenderEffectBloom : MonoBehaviour
+{
+
+    public Shader BloomShader;
+    [Range(0.0f, 100.0f)]
+    public float BloomFactor;
+    private Material screenMat;
+
+    Material ScreenMat
+    {
+        get
+        {
+            if (screenMat == null)
+            {
+                screenMat = new Material(BloomShader);
+                screenMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return screenMat;
+        }
+    }
+
+
+    void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        if (!BloomShader && !BloomShader.isSupported)
+        {
+            enabled = false;
+        }
+        BloomFactor = 0.0f;
+
+    }
+
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (BloomShader != null)
+        {
+            RenderTexture brightTexture = RenderTexture.GetTemporary(sourceTexture.width, sourceTexture.height);
+            RenderTexture blurTexture = RenderTexture.GetTemporary(sourceTexture.width, sourceTexture.height);
+
+            Graphics.Blit(sourceTexture, brightTexture, ScreenMat, 0);
+            ScreenMat.SetFloat("_Steps", BloomFactor);
+            Graphics.Blit(brightTexture, blurTexture, ScreenMat, 1);
+            ScreenMat.SetTexture("_BaseTex", sourceTexture);
+            Graphics.Blit(blurTexture, destTexture, ScreenMat, 2);
+
+            RenderTexture.ReleaseTemporary(brightTexture);
+            RenderTexture.ReleaseTemporary(blurTexture);
+
+        }
+        else
+        {
+            Graphics.Blit(sourceTexture, destTexture);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BloomFactor < 50) { BloomFactor = BloomFactor + 0.5f; }
+        else { BloomFactor = 0; }
+    }
+
+    void OnDisable()
+    {
+        if (screenMat)
+        {
+            DestroyImmediate(screenMat);
+        }
+    }
+}
---
+++ Assets/Shader/Outlined_Diffuse.shader
@@ -0,0 +1,85 @@
+﻿Shader "Outlined/Uniform"
+{
+	Properties
+	{
+		_Color("Main Color", Color) = (0.5,0.5,0.5,1)
+		_MainTex ("Texture", 2D) = "white" {}
+        _EmissiveColor( "Emissision Color",Color) = (1,1,1,1)
+        _Emission("Emission", Range(0,10)) = 10
+		_OutlineColor ("Outline color", Color) = (0,0,0,1)
+		_OutlineWidth ("Outlines width", Range (0.0, 2.0)) = 1.1
+	}
+
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	struct appdata
+	{
+		float4 vertex : POSITION;
+	};
+
+	struct v2f
+	{
+		float4 pos : POSITION;
+	};
+
+	uniform float _OutlineWidth;
+	uniform float4 _OutlineColor;
+	uniform sampler2D _MainTex;
+	uniform float4 _Color;
+    uniform float4 _EmissiveColor;
+    uniform float _Emission;
+	ENDCG
+
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" }
+
+		Pass //Outline
+		{
+			ZWrite Off
+			Cull Back
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			v2f vert(appdata v)
+			{
+				appdata original = v;
+				v.vertex.xyz += _OutlineWidth * normalize(v.vertex.xyz);
+
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				return o;
+
+			}
+
+			half4 frag(v2f i) : COLOR
+			{
+                float3 ambient = UNITY_LIGHTMODEL_AMBIENT.rgb;
+
+				return float4(_Emission * _EmissiveColor + ambient, 1.0)*_OutlineColor;
+			}
+
+			ENDCG
+		}
+
+		Tags{ "Queue" = "Geometry"}
+
+		CGPROGRAM
+		#pragma surface surf Lambert
+		 
+		struct Input {
+			float2 uv_MainTex;
+		};
+		 
+		void surf (Input IN, inout SurfaceOutput o) {
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
